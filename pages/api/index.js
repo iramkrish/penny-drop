22fa75f2574655ff095ad3d94bb53b8cdae663c8
ram krishnan <iramkrish95@gmail.com>
2023-11-11 10:02:43 +0530

    penny drop api finalised

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,5 @@
 let userAccounts = {
-    919890603333: {name:'ram krishnan konar',IFSC_Code:'HDFC0000490',amount:'1000'},
+    '9004891266HDFC0000490': {name:'ram krishnan konar',IFSC_Code:'HDFC0000490',amount:'1000'},
     user2: 500,
     user3: 750,
   };
@@ -8,23 +8,16 @@ export default function handler(req, res) {
     const amount = 1;
     if (req.method === 'POST') {
         const { receiver ,name, IFSC_Code } = req.body;
-        console.log(receiver ,name, IFSC_Code)
         // Check if sender and receiver exist in user accounts
-        if (!userAccounts[receiver]) {
+        if (!userAccounts[receiver+IFSC_Code]) {
           return res.status(400).json({ error: 'Invalid receiver' });
         }
 
-        // Check if sender has sufficient balance
-        // if (userAccounts[sender] < amount) {
-        //   return res.status(400).json({ error: 'Insufficient balance' });
-        // }
-
         // Perform penny drop transaction
-        // userAccounts[sender] -= amount;
         userAccounts[receiver] += amount;
 
-        res.json({ message: 'Penny drop successful' });
+        res.json({ account:receiver, message: 'Penny drop successful' });
     } else {
-        res.status(200).json({ name: 'John Doe' })
+        res.status(200).json({ name: 'Penny Drop API' })
     }
   }
